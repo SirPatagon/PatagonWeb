5733bdc6151e8ea919d54deb911334d1d592be6a
SirPatagon <sirpatagon+development@gmail.com>
2024-02-05 23:04:17 +0100

    Establish rough base theme

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img width="500" src="@/assets/logo.svg"/><br>
+    Welcome to my website!<br>
+    You can find me on:<br>
+    <br>
+    󰊤 GitHub&nbsp;&nbsp;&nbsp;=>
+    <a href="https://github.com/SirPatagon">https://github.com/SirPatagon</a><br>
+    <br>
+    󰮠 GitLab&nbsp;&nbsp;&nbsp;=>
+    <a href="https://gitlab.com/SirPatagon">https://gitlab.com/SirPatagon</a><br>
+    <br>
+    󰕄 Twitter&nbsp;&nbsp;=>
+    <a href="https://twitter.com/SirPatagon">https://twitter.com/SirPatagon</a><br>
+    <br>
+    󰫑 Mastodon&nbsp;=>
+    <a href="https://mastodon.social/@sirpatagon">https://mastodon.social/@sirpatagon</a><br>
+    <br>
+    󰓓 Steam&nbsp;&nbsp;&nbsp;&nbsp;=>
+    <a href="https://steamcommunity.com/id/SirPatagon/">https://steamcommunity.com/id/SirPatagon/</a><br>
+    <br>
+    󰗃 YouTube&nbsp;&nbsp;=>
+    <a href="https://www.youtube.com/channel/UClN8kD_2x5kIzmI852U4STw">https://www.youtube.com/channel/UClN8kD_2x5kIzmI852U4STw</a><br>
+    <br>
+    󰊫 Gmail&nbsp;&nbsp;&nbsp;&nbsp;=>
+    <a href="mailto:sirpatagon+development@gmail.com">mailto:sirpatagon+development@gmail.com</a><br>
+    <br>
+    <br>
+    visitor@sirpatagon.net:~$&nbsp;█
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
     name: 'HomeView',
     components: {
-        HelloWorld,
     },
 };
 </script>
+
+<style scoped lang="scss">
+.home {
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+}
+a {
+  white-space: nowrap;
+}
+</style>
