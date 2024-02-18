b9f469b50d69eb9f8eeaa31c8d3b67f7eeb89196
SirPatagon <sirpatagon+development@gmail.com>
2024-02-18 22:08:18 +0100

    Move background gradient with cursor

--- src/main.js
+++ src/main.js
@@ -5,3 +5,10 @@ import store from './store';
 import './styles/BaseStyles.scss';
 
 createApp(App).use(store).use(router).mount('#app');
+
+document.addEventListener('mousemove', e => {
+    setTimeout(() => {
+        document.body.parentElement.style.setProperty('--x', e.clientX + 'px');
+        document.body.parentElement.style.setProperty('--y', e.clientY + 'px');
+    }, 50);
+});
